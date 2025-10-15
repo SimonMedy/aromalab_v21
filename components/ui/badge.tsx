9a3e1740da16f938d49fcf72f2e605f7fbab255c
yxtomix <yxtomix@gmail.com>
2025-10-15 09:52:22 +0200

    Removed duplicate global.css

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -16,7 +16,7 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         warning:
-          'border-transparent bg-[#f97316] text-white hover:bg-[#ea580c]',
+          'border-transparent bg-warning text-white [a&]:hover:bg-warning/90 focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40 dark:bg-warning/60',
         outline:
           'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
       },
